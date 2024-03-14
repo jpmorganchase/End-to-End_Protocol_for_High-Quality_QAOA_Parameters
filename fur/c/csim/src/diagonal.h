cdfc98c25e1ffb1686fc8687b86a2030ebd85200
Tianyi Hao <tianyi.hao@wisc.edu>
2024-03-14 17:43:46 -0500

    remove fur

--- fur/c/csim/src/diagonal.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stddef.h>
-
-
-void apply_diagonal(double* sv_real, double* sv_imag, double theta, double* const diag, size_t n);
--- fur/simulator_base_torch.py
+++
@@ -1,9 +0,0 @@
-import typing
-import torch
-
-class QAOAFastSimulatorBase_torch(object):
-    def __init__(self, n_qubits: int, costs: torch.Tensor) -> None:
-        self.n_qubits = n_qubits
-        self.n_states = 2**n_qubits
-        self.hc_diag = costs
-        assert len(costs) == self.n_states
